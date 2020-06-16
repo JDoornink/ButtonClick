3c101d48ae41fbe04900de89aa3019b72113c92f
Josef <jdoorarg@gmail.com>
2020-06-16 07:13:20 -0700

    Initial Commit

---
+++ Pomodoro-NoStoryBoard1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pomodoro-NoStoryBoard1
+//
+//  Created by JD on 6/16/20.
+//  Copyright © 2020 JDoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
