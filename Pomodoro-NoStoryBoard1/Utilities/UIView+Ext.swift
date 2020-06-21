b8b5cc4c995bf26dcb81a49ff79e15bc7c006406
Josef <jdoorarg@gmail.com>
2020-06-20 17:33:04 -0700

    additional files for TableView POC

---
+++ Pomodoro-NoStoryBoard1/Utilities/UIView+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Ext.swift
+//  Pomodoro-NoStoryBoard1
+//
+//  Created by JD on 6/20/20.
+//  Copyright © 2020 JDoor. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+    }
+}
